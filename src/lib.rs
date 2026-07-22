79119975c45499e66b1977172610adbe5b0118af
Jake Stewart <jake.stewart@racelogic.co.uk>
2026-07-22 19:25:46 +0100

    feat: add lap timing and time-series analysis

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,15 @@
 
 #![forbid(unsafe_code)]
 
+mod analysis;
 mod parser;
 mod telemetry;
 mod types;
 
+pub use analysis::{
+    align_channels, detect_laps, AlignedChannel, AnalysisError, Gate, GateDirection, Lap,
+    LapConfig, SectorGate, SectorTime, TimeSeries, TimedPoint, MAX_RESAMPLED_SAMPLES,
+};
 pub use parser::{parse_path, ParseOptions, Parser};
 pub use telemetry::{
     packed_minutes_to_degrees, CoordinateAxis, GeoPoint, SatelliteQuality, SessionMetrics,
